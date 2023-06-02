50902d85cbf3d07ff3a3023d0aad03c414da1c23
ulxsth <ulxsth@gmail.com>
2023-06-03 02:36:40 +0900

    update:シングルトン化

---
+++ src/main/java/com/ulxsth/thebout/TheBoutPlugin.java
@@ -0,0 +1,21 @@
+package com.ulxsth.thebout;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TheBoutPlugin extends JavaPlugin {
+    private static TheBoutPlugin plugin = null;
+
+    public static TheBoutPlugin getInstance() {
+        if(plugin == null) {
+            throw new IllegalStateException("プラグインが初期化されていません");
+        }
+        return plugin;
+    }
+
+    @Override
+    public void onEnable() {
+        if(plugin == null) {
+            plugin = this;
+        }
+    }
+}
